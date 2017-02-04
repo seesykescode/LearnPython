35f59e26e067297551949a40c7870a4175927224
Johnathon Sykes <johndsykes86@gmail.com>
2017-02-03 22:09:20 -0800

    Exercises 1-3 from Learn Python The Hard Way

---
+++ ex3.py
@@ -0,0 +1,40 @@
+print "I will not count my chickens: "
+# ouputs the following string to console "I will not count my chickens"
+
+print "Hens", 25.0 + 30.0 / 6.0
+# prints out "Hens" then divides 6 from 30, which is 5 and then adds 25. The answer is 30 and gets printed to the terminal as well
+
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+# prints the word "Roosters" out to terminal then multiplies 25 * 3 = 75 then takes that answer and divides that by 4 and searchs for a reminder. That answer is 3. So you're left with 100 - 3 which is 97. Woof. That was more typing than I bargained for.
+
+print "Now I will count the eggs: "
+# prints "Now I will count the eggs: "
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0+ 6.0
+# prints the answer to the above math. I'm not breaking that down.
+
+print "Is it true that 3 + 2 < 5 - 7"
+# prints "Is it true that 3+2<5-7" and returns False
+
+print 3.0 + 2.0 < 5.0 - 7.0
+# why didn't this print? The world may never know...but it did seem to return False. We'll need to look into this.
+
+print "What is 3 + 2?", 3.0 + 2.0
+# prints "What is 3 + 2?" and returns 3+2 which equals 5
+print "What is 5 - 7? ", 5.0 - 7.0
+# prints out "What is 5 - 7" to the terminal and returns the answer 5 - 7 which equals -2
+
+print "Oh, that's why it's False."
+# prints out the words "Oh, that's why it's False."
+
+print "How about some more."
+# prints out the worlds "How about some more."
+
+print "Is it greater?", 5.0 > -2.0
+# prints out the words "Is it greater?" and then calculates whether 5 > -2 is true...which is True and returns output to the terminal as True.
+
+print "Is it greater or equal?", 5.0 >= -2.0
+# prints out the words "Is it greater or equal?" and then evaulates whether 5 is greater than or equal to -2 which is true. Thus, it returns True.
+
+print "Is it less or equal?", 5.0 <= -2.0
+# prints out "Is it less or equal" to the terminal and then evaulates whether 5 is less than or equal to -2, which it is now. It returns False to the terminal
---
+++ ex3a.py
@@ -0,0 +1,1 @@
+print "How many hours are in a year?", 365 * 24
